b012b19027c4beeed147497ef48d3e910617e9ad
ChrisDavis04 <christopher.davis@bsu.edu>
2025-02-17 19:10:44 -0500

    Added comments and added methods building the gui.

--- src/main/java/bsu/edu/cs/WikipediaGUI.java
+++ src/main/java/bsu/edu/cs/WikipediaGUI.java
@@ -2,10 +2,7 @@ package bsu.edu.cs;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.List;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
